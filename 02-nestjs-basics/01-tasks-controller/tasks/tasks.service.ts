e41b08abc16153479ea20ddf278c4f1203f57aae
Николай Лукиных <jobnikalun@gmail.com>
2024-12-19 20:36:10 +0300

    add solution for 2-module 1-task-contoller: create api

--- 02-nestjs-basics/01-tasks-controller/tasks/tasks.service.ts
+++ 02-nestjs-basics/01-tasks-controller/tasks/tasks.service.ts
@@ -1,17 +1,68 @@
-import { Injectable, NotFoundException } from "@nestjs/common";
+import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
 import { Task } from "./task.model";
 
 @Injectable()
 export class TasksService {
   private tasks: Task[] = [];
 
-  getAllTasks(): Task[] {}
+  getAllTasks(): Task[] {
+    return this.tasks;
+  }
 
-  getTaskById(id: string): Task {}
+  getTaskById(id: string) {
+    const task = this.tasks.find(item => item.id === id);
+    if (!task) {
+      throw new HttpException({
+        status: HttpStatus.NOT_FOUND,
+        error: 'Такого элемента не существует',
+      }, HttpStatus.NOT_FOUND);
+    }
 
-  createTask(task: Task): Task {}
+    return task;
+  }
 
-  updateTask(id: string, update: Task): Task {}
+  createTask(task: Task) {
+    const createdTask = {
+        ...task,
+        id: String(this.tasks.length + 1),
+    }
+    this.tasks.push(createdTask);
+    return createdTask;
+  }
 
-  deleteTask(id: string): Task {}
+  updateTask(id: string, update: Task) {
+      const index = this.tasks.findIndex(item => item.id === id);
+
+   if (index === -1) {
+      throw new HttpException({
+        status: HttpStatus.NOT_FOUND,
+        error: 'Такого элемента не существует',
+      }, HttpStatus.NOT_FOUND);
+    }
+
+    const updatedTask: Task = {
+      id,
+      ...update,
+    }
+
+    this.tasks.splice(index, 1, updatedTask);
+
+    return updatedTask;
+  }
+
+  deleteTask(id: string) {
+    const index = this.tasks.findIndex(item => item.id === id);
+
+    if (index === -1) {
+      throw new HttpException({
+        status: HttpStatus.NOT_FOUND,
+        error: 'Такого элемента не существует',
+      }, HttpStatus.NOT_FOUND);
+    }
+
+    const deleted = this.tasks[index];
+    this.tasks.splice(index, 1);
+
+    return deleted;
+  }
 }
