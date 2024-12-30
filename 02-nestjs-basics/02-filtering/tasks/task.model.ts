1dfd681d7ed11fa1129d331c44784682dccd339f
Николай Лукиных <jobnikalun@gmail.com>
2024-12-30 17:24:10 +0300

    add solution for 2-module 02-filtering: filtration and pagination

--- 02-nestjs-basics/02-filtering/tasks/task.model.ts
+++ 02-nestjs-basics/02-filtering/tasks/task.model.ts
@@ -1,3 +1,6 @@
+import { IsEnum, IsOptional, IsPositive } from "class-validator";
+import { Type } from "class-transformer";
+
 export enum TaskStatus {
   PENDING = "pending",
   IN_PROGRESS = "in_progress",
@@ -9,4 +12,32 @@ export interface Task {
   title: string;
   description: string;
   status: TaskStatus;
+}
+
+enum SortByFields {
+  id = 'id',
+  description = 'description',
+}
+
+export interface QueryParams {
+  status?: TaskStatus;
+  page?: number;
+  limit?: number;
+  sortBy?: SortByFields;
+}
+
+export class QueryParamsValidator implements QueryParams {
+  @IsEnum(TaskStatus)
+  @IsOptional()
+  status?: TaskStatus;
+  @Type(() => Number)
+  @IsPositive()
+  @IsOptional()
+  page?: number;
+  @Type(() => Number)
+  @IsPositive()
+  @IsOptional()
+  limit?: number;
+  @IsEnum(SortByFields)
+  sortBy?: SortByFields;
 }
