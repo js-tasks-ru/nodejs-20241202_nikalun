b98776f52f8cd1a406fc620db440fd8fa5eb62ee
Николай Лукиных <jobnikalun@gmail.com>
2025-01-25 12:43:25 +0300

    add solution for 5-module 01-crud-tasks: create db connection and all need methods. Added pagination and limit fot method @Get findAll
    (cherry picked from commit f6d14fc88fde7bc07880cdaee3fe05abfbdae88d)

--- 05-db/01-crud-tasks/tasks/dto/update-task.dto.ts
+++ 05-db/01-crud-tasks/tasks/dto/update-task.dto.ts
@@ -1,4 +1,10 @@
 import { PartialType } from "@nestjs/mapped-types";
 import { CreateTaskDto } from "./create-task.dto";
+import { IsString } from "class-validator";
 
-export class UpdateTaskDto extends PartialType(CreateTaskDto) {}
+export class UpdateTaskDto extends PartialType(CreateTaskDto) {
+    @IsString()
+    title: string;
+    @IsString()
+    description: string;
+}
