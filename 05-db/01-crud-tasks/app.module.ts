0bc7aae51416e26d4577152e878b63b7d3dc4c0b
Николай Лукиных <jobnikalun@gmail.com>
2025-01-31 11:57:33 +0300

    add solution for 5-module 01-crud-tasks: created methods for db sqlite

--- 05-db/01-crud-tasks/app.module.ts
+++ 05-db/01-crud-tasks/app.module.ts
@@ -1,7 +1,16 @@
 import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
 import { TasksModule } from "./tasks/tasks.module";
+import { Task } from "./tasks/entities/task.entity";
 
 @Module({
-  imports: [TasksModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: "sqlite",
+      database: 'db.sqlite',
+      entities: [Task],
+      //synchronize: true,
+    }),
+    TasksModule],
 })
 export class AppModule {}
